4d74d57a93e1028c990cd9bcc9e8f8a8635bf1fa
abdullrhman <abdulllrhmannn@gmail.noreply.github.com>
2023-09-20 07:52:03 +0300

    add functionality to the UI code

--- javascript.js
+++ javascript.js
@@ -1,51 +1,125 @@
+
 function getComputerChoice(){
-    let choice = Math.floor(Math.random() * 3);
-    
-    if(choice === 0)return 'Rock';
-    else if(choice === 1)return 'Paper';
-    else return 'Scissors';
+    let num = Math.floor(Math.random() * 3);
+    const choice = ['Rock' , 'Paper' , 'Scissors']
+    return choice[num];
 }
 
 
 function playRound(playerSelection, computerSelection) {
 
-    if(playerSelection === computerSelection)return "It's a Draw !";
+    if(playerSelection === computerSelection)
+        return `It's a Tie!`;
     else if( 
         playerSelection === 'rock' && computerSelection !== 'scissors' ||
         playerSelection === 'scissors' && computerSelection !== 'paper' ||
         playerSelection === 'paper' && computerSelection !== 'rock' 
-      )
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+
+    ) return `You Lost!`;
+    else return 'You Won!';
+}
+
+function getWinner(round){
+    if(round.includes("Won"))return 1;
+    else if(round.includes("Lost"))return -1;
+    else return 0;
+}
+
+function displayRoundDescription(roundResult , playerSelection , computerSelection){
+
+    let roundText = document.querySelector('#round-result #result');
+    roundText.textContent = roundResult;
+
+    let roundDescription = document.querySelector('#round-result #description')
+
+    if(roundResult.includes("Lost"))
+        roundDescription.textContent = `${playerSelection} is beaten by ${computerSelection}`;
+    else if(roundResult.includes("Won"))
+        roundDescription.textContent = `${playerSelection} beats ${computerSelection}`;
+    else roundDescription.textContent = `${playerSelection} ties with ${computerSelection}`;
+
+}
+
+function displayRoundScore(playerScore , computerScore , playerSelection , computerSelection)
+{
+    let playerText = document.querySelector('#player .text');
+    let playerSign = document.querySelector('#player .sign');
+
+    playerText.textContent = `Player: ${playerScore}`;
+    playerSign.textContent = document.querySelector(`#${playerSelection} .sign`).textContent;
+
+
+    let computerText = document.querySelector('#computer .text');
+    let computerSign = document.querySelector('#computer .sign');
+
+    computerText.textContent = `Computer: ${computerScore}`;
+    computerSign.textContent = document.querySelector(`#${computerSelection} .sign`).textContent;
+
+}
+
+function game(e){
+
+    this.classList.add('click');
+    roundNum ++;
+
+    let computerSelection = getComputerChoice().toLowerCase();
+    let playerSelection = this.id;
+
+    console.log(playerSelection);
+    console.log(computerSelection);
+
     
-    return 'You Won !';
+    let roundResult = playRound(playerSelection , computerSelection);
+
+    let flg =  getWinner(roundResult);
+    if(flg === 1)playerScore++;
+    else if(flg === -1)computerScore++;
+
+    displayRoundScore(playerScore , computerScore , this.id , computerSelection);
+    displayRoundDescription(roundResult , playerSelection , computerSelection);
+    
 }
 
-function game(){
+function setBtnEvents(){
+    const btns = document.querySelectorAll('.btn');
 
-    function getWinner(round){
-        if(round.includes("Won"))return 1;
-        else if(round.includes("Lose"))return -1;
-        else return 0;
-    }
+    // add hover event
+    btns.forEach(btn => btn.addEventListener(
+        'mouseover' ,
+        function(e){
+            this.classList.add('hover');
 
-    let humanScore = 0 , computerScore = 0;
-    for(let i = 0 ; i < 5 ; i++){
+        }));
 
-        let computerSelection = getComputerChoice().toLowerCase();
-        let playerSelection = prompt("enter your selection : ").toLowerCase();
+    // remove hover event
+    btns.forEach(btn => btn.addEventListener(
+        'mouseleave' ,
+        function(e){
+            this.classList.remove('hover');
 
-        let roundResult = playRound(computerSelection , playerSelection);
-        console.log(`Round ${i+1} : ${roundResult}`);
+        }));
 
-        
+    // add click event
+    btns.forEach(btn => btn.addEventListener(
+            'click' ,
+            game
+        ));
 
-        let flg =  getWinner(roundResult);
-        if(flg === 1)humanScore++;
-        else if(flg === -1)computerScore++;
-    }
+    // remove click event
+    btns.forEach(btn => btn.addEventListener(
+        'mouseout' ,
+        function(e){
+            this.classList.remove('click');
 
-    if(humanScore > computerScore)console.log("You Won !");
-    else if(humanScore > computerScore)console.log("You Lose !");
-    else console.log("It's a Draw");
+        }));
 }
+
+
+let playerScore = 0 , computerScore = 0 , roundNum = 0;
+let playerBtn = 
+
+setBtnEvents();
+
+
+
 
